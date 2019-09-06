bacc1c5f25dd927218579c90bcaab55aba8af9ba
simsolam <49168585+simsolam@users.noreply.github.com>
2019-09-05 21:45:13 -0500

    Lab2

---
+++ Lab 2/Source Code and Outputs/LabC/WordCount.java
@@ -0,0 +1,119 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool
+{
+
+	public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+	{
+
+		private boolean caseSensitive = false;
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		protected void setup(Mapper.Context context)
+			      throws IOException,
+			        InterruptedException {
+			      Configuration config = context.getConfiguration();
+			      this.caseSensitive = config.getBoolean("wordcount.case.sensitive", false);
+			    }
+		
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+		{
+			String line = value.toString();
+		      if (!caseSensitive) {
+		        line = line.toLowerCase();
+		      }
+			for (String token : line.split("\\s+"))
+			{
+				if(token.equalsIgnoreCase("hadoop")||token.equals("java")){
+				word.set(token);
+				context.write(word, one);
+				}
+			}
+		}
+	}
+
+	public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		private IntWritable result = new IntWritable();
+
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+		{
+			int sum = 0;
+			for (IntWritable val : values)
+			{
+				sum += val.get();
+			}
+			
+			
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+
+		int res = ToolRunner.run(conf, new WordCount(), args);
+
+		System.exit(res);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception
+	{
+
+		Job job = new Job(getConf(), "WordCount");
+		job.setJarByClass(WordCount.class);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		
+		Configuration conf = new Configuration();
+		//Creating FileSystem object with the configuration
+		FileSystem fs = FileSystem.get(conf);
+		//Check if output path (args[1])exist or not
+		if(fs.exists(new Path(args[1]))){
+		   /*If exist delete the output path*/
+		   fs.delete(new Path(args[1]),true);
+		}
+		
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));		
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+	
+}
